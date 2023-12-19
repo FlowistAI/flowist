2026a36ad3ed2a7e230d9d843ef2856e7ad7b0ca
pluveto <i@pluvet.com>
2023-12-19 17:27:51 +0800

    chore: add padding rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -31,6 +31,18 @@ module.exports = {
     'react-refresh/only-export-components': [
       'warn',
       { 'allowConstantExport': true }
-    ]
+    ],
+    // padding line after import, export, if-else, loop, func
+    'padding-line-between-statements': [
+      'error',
+      { 'blankLine': 'always', 'prev': '*', 'next': 'return' },
+      { 'blankLine': 'always', 'prev': '*', 'next': 'export' },
+      { 'blankLine': 'always', 'prev': '*', 'next': 'for' },
+      { 'blankLine': 'always', 'prev': '*', 'next': 'while' },
+      { 'blankLine': 'always', 'prev': '*', 'next': 'function' },
+      { 'blankLine': 'always', 'prev': '*', 'next': 'try' },
+      { 'blankLine': 'always', 'prev': '*', 'next': 'switch' },
+      { 'blankLine': 'always', 'prev': 'block-like', 'next': '*' },
+    ],
   }
 };
