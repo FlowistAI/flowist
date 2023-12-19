30fe5065b1fdd8193e5efd240eea408b314024b7
pluveto <i@pluvet.com>
2023-12-19 17:21:18 +0800

    chore: update eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -10,9 +13,24 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        'multiline': {
+          'delimiter': 'none',
+          'requireLast': true
+        },
+        'singleline': {
+          'delimiter': 'semi',
+          'requireLast': false
+        }
+      }
+    ],
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+      { 'allowConstantExport': true }
+    ]
+  }
+};
