1f290944c63475310fb8921816de79f575d4364d
pluveto <i@pluvet.com>
2023-12-25 17:48:22 +0800

    feat: avoid unintended touch in useSettingsModal

--- src/pages/setting/useSettingsModal.tsx
+++ src/pages/setting/useSettingsModal.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { Modal } from '@mui/joy'
 import { useAtom } from 'jotai'
 import { settingsModalOpenAtom } from '../../states/settings/settings.atom'
@@ -5,11 +6,33 @@ import { SettingPage } from './SettingPage'
 
 export const SettingsModal = () => {
     const [open, setOpen] = useAtom(settingsModalOpenAtom)
+    const [disableBackdropClick, setDisableBackdropClick] = useState(true)
+
+    // avoid unintended touch
+    useEffect(() => {
+        if (open) {
+            setDisableBackdropClick(true)
+            const timer = setTimeout(() => {
+                setDisableBackdropClick(false)
+            }, 500)
+
+            return () => clearTimeout(timer)
+        }
+    }, [open])
 
     const handleClose = () => setOpen(false)
 
     return (
-        <Modal open={open} onClose={handleClose}>
+        <Modal
+            open={open}
+            onClose={(_, reason) => {
+                if (reason === 'backdropClick' && disableBackdropClick) {
+                    return
+                }
+
+                handleClose()
+            }}
+        >
             <div
                 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white shadow-md p-4"
                 style={{ width: '40rem' }}
@@ -22,4 +45,3 @@ export const SettingsModal = () => {
         </Modal>
     )
 }
-
